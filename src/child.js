cfcaf0c3267f41e1009c3bd065846f82b00ed55d
ehmicky <ehmicky@gmail.com>
2025-09-28 22:07:12 +0100

    Add comments

--- src/child.js
+++ src/child.js
@@ -1,11 +1,14 @@
 import { setNonEnumProp } from './utils.js'
 
+// Child errors (`cause` and `errors`) are printed on their own after their
+// parent, with some indentation.
+// They are temporarily removed so that they are not printed twice.
 export const pickChildErrors = (error) => {
   const { cause, errors } = error
   // eslint-disable-next-line fp/no-delete
   delete error.cause
   // eslint-disable-next-line fp/no-delete
-  delete error.error
+  delete error.errors
   return { cause, errors }
 }
 
