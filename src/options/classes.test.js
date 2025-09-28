02acb7b85fbdf04531a62aedc8bf4a28e6da2bd7
ehmicky <ehmicky@gmail.com>
2025-09-28 23:33:49 +0100

    Add more tests

--- src/options/classes.test.js
+++ src/options/classes.test.js
@@ -32,3 +32,50 @@ test('Can set classes.* undefined', (t) => {
   })
   t.true(message.includes(`${figures.warning} TypeError: test`))
 })
+
+test('Can set classes.* empty', (t) => {
+  const message = beautifulError(new TypeError('test'), {
+    classes: { TypeError: {} },
+  })
+  t.true(message.includes(`${figures.cross} TypeError: test`))
+})
+
+test('Can set classes.default', (t) => {
+  const message = beautifulError(new TypeError('test'), {
+    classes: { default: { icon: 'warning' } },
+  })
+  t.true(message.includes(`${figures.warning} TypeError: test`))
+})
+
+test('Can set classes.default that overrides', (t) => {
+  const message = beautifulError(new TypeError('test'), {
+    icon: 'info',
+    classes: { default: { icon: 'warning' } },
+  })
+  t.true(message.includes(`${figures.warning} TypeError: test`))
+})
+
+test('Can set classes.default that is overridden', (t) => {
+  const message = beautifulError(new TypeError('test'), {
+    classes: {
+      default: { icon: 'info' },
+      TypeError: { icon: 'warning' },
+    },
+  })
+  t.true(message.includes(`${figures.warning} TypeError: test`))
+})
+
+test('Can set classes.default undefined', (t) => {
+  const message = beautifulError(new TypeError('test'), {
+    icon: 'warning',
+    classes: { default: { icon: undefined } },
+  })
+  t.true(message.includes(`${figures.warning} TypeError: test`))
+})
+
+test('Can set classes.default empty', (t) => {
+  const message = beautifulError(new TypeError('test'), {
+    classes: { default: {} },
+  })
+  t.true(message.includes(`${figures.cross} TypeError: test`))
+})
